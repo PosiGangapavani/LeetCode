39eab9505e5fd49b5cdc6a5b52c4b01d2aefb3f7
Yelubandi Posi Ganga Pavani <88924871+PosiGangapavani@users.noreply.github.com>
2024-12-20 08:02:13 +0530

    Time: 0 ms, Memory: 23.1 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,16 +1,7 @@
 class Solution:
-    def helper(self,idx,s,ans):
-        if idx >= len(s) or len(s) == 0:
-            return 
-        self.helper(idx + 1 , s,ans)
-        ans.append(s[idx])
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        ans = []
-        self.helper(0,s,ans)
-        for i in range(len(ans)):
-            s[i] = ans[i]
-        
+        s.reverse()
         
